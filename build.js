2e66ac00fb03f7dbcb76c6e5c6cb830164195f9c
Tyler Nickerson <nickersoft@gmail.com>
2018-09-29 11:59:59 -0700

    Updated package.json and build script to include redirects file

--- build.js
+++ build.js
@@ -4,24 +4,27 @@ function runGatsby(locale) {
   shell.exec(`LOCALE=${locale} ./node_modules/.bin/gatsby build --prefix-paths`);
 }
 
+// Removes existing cache
 shell.rm('-rf', '.cache');
 
-console.log('\n🇺🇸 Building US site...\n');
-
+// Scaffold directories 
 shell.mkdir('-p', 'dist/fr', 'dist/en');
 
+// Build the English version of the site
+console.log('\n🇺🇸 Building US site...\n');
 runGatsby('en');
-
 shell.cp('-r', './public/.', './dist/en');
 
+// Build the French version of the site
 console.log('\n🇫🇷 Building French site...\n');
-
 runGatsby('fr');
-
 shell.cp('-r', './public/.', './dist/fr');
 
-console.log('\n🗑 Cleaning up...\n');
+// Copy redirects file
+shell.cp('./_redirects', './dist')
 
+// Clean up
+console.log('\n🗑 Cleaning up...\n');
 shell.rm('-rf', 'public');
 
 console.log('✨ Done!');
